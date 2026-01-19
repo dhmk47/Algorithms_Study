3062f8f8499de2d5a936085f99944b802c288c7d
unknown <dhmk47@naver.com>
2026-01-19 20:18:04 +0900

    인프런 코딩테스트

---
+++ src/inflearn/java_algorithms/CombiningTwoArrangements_3_1.java
@@ -0,0 +1,62 @@
+package inflearn.java_algorithms;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class CombiningTwoArrangements_3_1 {
+	public static void main(String[] args) {
+		CombiningTwoArrangements_3_1 prc = new CombiningTwoArrangements_3_1();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int n = Integer.parseInt(br.readLine());
+			int[] firstArray = new int[n];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for(int i = 0; i < n; i++) {
+				firstArray[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int m = Integer.parseInt(br.readLine());
+			int[] secondArray = new int[m];
+			st = new StringTokenizer(br.readLine());
+			
+			for(int i = 0; i < m; i++) {
+				secondArray[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			bw.write(prc.solution(n, m, firstArray, secondArray));
+			bw.flush();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String solution(int n, int m, int[] firstArray, int[] secondArray) {
+		StringBuilder answer = new StringBuilder();
+		int p1 = 0;
+		int p2 = 0;
+		
+		while(p1 < n && p2 < m) {
+			if(firstArray[p1] < secondArray[p2]) {
+				answer.append(firstArray[p1++] + " ");
+			}else {
+				answer.append(secondArray[p2++] + " ");
+			}
+		}
+		
+		while(p1 < n) {
+			answer.append(firstArray[p1++] + " ");
+		}
+		while(p2 < m) {
+			answer.append(secondArray[p2++] + " ");
+		}
+		
+		return answer.toString().trim();
+	}
+}
---
+++ src/inflearn/java_algorithms/Mentoring_2_12.java
@@ -0,0 +1,57 @@
+package inflearn.java_algorithms;
+import java.io.*;
+import java.util.StringTokenizer;
+public class Mentoring_2_12 {
+	public static void main(String[] args) {
+		Mentoring_2_12 prc = new Mentoring_2_12();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());		// 학생 수
+			int m = Integer.parseInt(st.nextToken());		// 케이스
+			int[][] arr = new int[m][n];
+			
+			for(int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < n; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			bw.write(String.valueOf(prc.solution(n, m, arr)));
+			bw.flush();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private int solution(int n, int m, int[][] arr) {
+		int answer = 0;
+		int[][] resultArray = new int[n + 1][n + 1];
+		
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				int studentNumber = arr[i][j];
+				for(int k = 0; k < n; k++) {
+					if(j < k && resultArray[studentNumber][arr[i][k]] == 0) {		// 0일 때만 
+						resultArray[studentNumber][arr[i][k]] = 1;
+					}else if(j > k) {												// 한 번이라도 성적이 낮다면 -1로 넣어줌
+						resultArray[studentNumber][arr[i][k]] = -1;
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < resultArray.length; i++) {
+			for(int j = 0; j < resultArray[i].length; j++) {
+				if(resultArray[i][j] == 1) {
+					answer++;
+				}
+			}
+		}
+		return answer;
+	}
+}
